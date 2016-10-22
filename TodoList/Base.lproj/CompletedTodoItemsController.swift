f0395219a9f11f8d508ad1ce6b5e4cb67b98e7e0
Eric Groom <ericgroom@Erics-MacBook-Air.local>
2016-10-22 10:23:18 -0700

    Add CompletedTodoItemsController

---
+++ TodoList/Base.lproj/CompletedTodoItemsController.swift
@@ -0,0 +1,25 @@
+//
+//  CompletedTodoListController.swift
+//  TodoList
+//
+//  Created by Eric Groom on 10/21/16.
+//  Copyright © 2016 Eric Groom. All rights reserved.
+//
+
+import UIKit
+
+class CompletedTodoListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
